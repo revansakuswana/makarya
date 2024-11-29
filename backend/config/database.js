b3a5c3cc7680699780ba8585a400eef5afc5bc35
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2024-11-29 23:31:55 +0700

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log(process.env.DB_PORT)
+
 const db = new Sequelize(
   process.env.DB_DATABASE || "makarya",
   process.env.DB_USERNAME || "root",
