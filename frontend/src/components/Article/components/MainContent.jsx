b44c88b28b363ec6435f30ddc3cea7237a93db0a
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2024-12-04 23:45:13 +0700

    fix: enhance error handling in user authentication process

--- frontend/src/components/Article/components/MainContent.jsx
+++ frontend/src/components/Article/components/MainContent.jsx
@@ -7,11 +7,14 @@ import {
   parseISO,
 } from "date-fns";
 import {
+  ThemeProvider,
+  createTheme,
   styled,
   Box,
   Button,
   Typography,
   Snackbar,
+  Pagination,
   Alert,
   Avatar,
   Card,
@@ -23,11 +26,12 @@ import {
 } from "@mui/material";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { marked } from "marked";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid2";
 import PropTypes from "prop-types";
-import axios from "axios";
+import getBlogTheme from "../theme/getBlogTheme";
 import Loaders from "../../Loaders/Loaders";
+import axios from "axios";
 
 const SyledCard = styled(Card)(({ theme }) => ({
   display: "flex",
@@ -108,11 +112,16 @@ Search.propTypes = {
 };
 
 export default function MainContent() {
+  const defaultTheme = createTheme();
+  const blogTheme = createTheme(getBlogTheme("light"));
+
   const [articles, setArticles] = React.useState([]);
   const [categories, setCategories] = React.useState([]);
   const [selectedCategory, setSelectedCategory] =
     React.useState("Semua Kategori");
   const [searchQuery, setSearchQuery] = React.useState("");
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [articlesPerPage] = React.useState(9);
 
   const [loading, setLoading] = useState(true);
   const [alertOpen, setAlertOpen] = useState(false);
@@ -150,7 +159,11 @@ export default function MainContent() {
         const response = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/api/allarticles`
         );
-        setCategories(response.data.data);
+        const uniqueCategories = Array.from(
+          new Set(response.data.data.map((article) => article.category))
+        );
+
+        setCategories(uniqueCategories);
       } catch (err) {
         setAlertMessage("Terjadi kesalahan saat mengambil data");
         setAlertSeverity("error");
@@ -211,7 +224,9 @@ export default function MainContent() {
     if (isLoggedIn) {
       navigate("/articles/articlesform");
     } else {
-      navigate("/users/signin", { state: { redirect: "/articles/articlesform" } });
+      navigate("/users/signin", {
+        state: { redirect: "/articles/articlesform" },
+      });
     }
   };
 
@@ -233,6 +248,20 @@ export default function MainContent() {
     return matchesCategory && matchesSearchQuery;
   });
 
+  // Menambahkan logika pagination ke filteredArticles
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = filteredArticles.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
+
+  const totalPages = Math.ceil(filteredArticles.length / articlesPerPage);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   const getExcerpt = (content, maxLength = 100) => {
     // Mengonversi Markdown ke HTML dan kemudian menggunakannya untuk mendapatkan excerpt
     const htmlContent = marked(content);
@@ -243,126 +272,41 @@ export default function MainContent() {
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}>
-        <Typography
-          sx={{ marginBottom: 0, fontSize: { xs: 23, md: 40 } }}
-          variant="h2"
-          gutterBottom>
-          Rekomendasi Artikel
-        </Typography>
-
-        <Button
-          sx={{ fontWeight: "bold", fontSize: { xs: 13, md: 15 } }}
-          color="secondary"
-          variant="contained"
-          size="small"
-          onClick={handleCreateArticleClick}>
-          <PencilSquareIcon
-            style={{
-              height: 20,
-              width: 20,
-              marginRight: 5,
-            }}
-          />
-          Buat Artikel
-        </Button>
-      </Box>
-
-      <Box
-        sx={{
-          display: { xs: "flex", sm: "none" },
-          flexDirection: "row",
-          gap: 1,
-          width: { xs: "100%", md: "fit-content" },
-          overflow: "auto",
-        }}>
-        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column-reverse", md: "row" },
-          justifyContent: "space-between",
-          alignItems: { xs: "start", md: "center" },
-          gap: 4,
-          overflow: "auto",
-        }}>
+    <ThemeProvider theme={blogTheme}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
         <Box
           sx={{
-            display: "inline-flex",
-            flexDirection: "row",
-            gap: 3,
-            overflow: "auto",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}>
-          <Chip
-            onClick={() => handleClick("Semua Kategori")}
-            size="medium"
-            label="Semua Kategori"
-            color={
-              selectedCategory === "Semua Kategori" ? "primary" : "default"
-            }
-            variant={
-              selectedCategory === "Semua Kategori" ? "contained" : "outlined"
-            }
-            sx={{
-              backgroundColor:
-                selectedCategory === "Semua Kategori"
-                  ? "primary.main"
-                  : "transparent",
-              color:
-                selectedCategory === "Semua Kategori"
-                  ? "white"
-                  : "text.primary",
-              "&:hover": {
-                backgroundColor:
-                  selectedCategory === "Semua Kategori"
-                    ? "primary.dark"
-                    : "grey.300",
-              },
-            }}
-          />
-          {categories.map((category) => (
-            <Chip
-              key={category.id}
-              onClick={() => handleClick(category.category)}
-              size="medium"
-              label={category.category}
-              color={
-                selectedCategory === category.category ? "primary" : "default"
-              }
-              variant={
-                selectedCategory === category.category
-                  ? "contained"
-                  : "outlined"
-              }
-              sx={{
-                backgroundColor:
-                  selectedCategory === category.category
-                    ? "primary.main"
-                    : "transparent",
-                color:
-                  selectedCategory === category.category
-                    ? "white"
-                    : "text.primary",
-                "&:hover": {
-                  backgroundColor:
-                    selectedCategory === category.category
-                      ? "primary.dark"
-                      : "grey.300",
-                },
+          <Typography
+            sx={{ marginBottom: 0, fontSize: { xs: 23, md: 40 } }}
+            variant="h2"
+            gutterBottom>
+            Rekomendasi Artikel
+          </Typography>
+
+          <Button
+            sx={{ fontWeight: "bold", fontSize: { xs: 13, md: 15 } }}
+            color="secondary"
+            variant="contained"
+            size="small"
+            onClick={handleCreateArticleClick}>
+            <PencilSquareIcon
+              style={{
+                height: 20,
+                width: 20,
+                marginRight: 5,
               }}
             />
-          ))}
+            Buat Artikel
+          </Button>
         </Box>
+
         <Box
           sx={{
-            display: { xs: "none", sm: "flex" },
+            display: { xs: "flex", sm: "none" },
             flexDirection: "row",
             gap: 1,
             width: { xs: "100%", md: "fit-content" },
@@ -370,85 +314,185 @@ export default function MainContent() {
           }}>
           <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         </Box>
-      </Box>
-      {loading ? (
-        <div className="flex justify-center items-center h-full my-72">
-          <Loaders size={70} />
-        </div>
-      ) : (
-        <Grid container spacing={2} columns={12}>
-          {filteredArticles.map((articles, index) => (
-            <Grid key={index} size={{ xs: 12, sm: 6, md: 4 }}>
-              <SyledCard tabIndex={0}>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={`${import.meta.env.VITE_BASE_URL}/public/images/${
-                    articles.image
-                  }`}
-                  alt="image cover"
-                />
-                <SyledCardContent>
-                  <Typography gutterBottom variant="caption" component="div">
-                    {articles.category}
-                  </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column-reverse", md: "row" },
+            justifyContent: "space-between",
+            alignItems: { xs: "start", md: "center" },
+            gap: 4,
+            overflow: "auto",
+          }}>
+          <Box
+            sx={{
+              display: "inline-flex",
+              flexDirection: "row",
+              gap: 3,
+              overflow: "auto",
+            }}>
+            <Chip
+              onClick={() => handleClick("Semua Kategori")}
+              size="medium"
+              label="Semua Kategori"
+              color={
+                selectedCategory === "Semua Kategori" ? "primary" : "default"
+              }
+              variant={
+                selectedCategory === "Semua Kategori" ? "contained" : "outlined"
+              }
+              sx={{
+                backgroundColor:
+                  selectedCategory === "Semua Kategori"
+                    ? "primary.main"
+                    : "transparent",
+                color:
+                  selectedCategory === "Semua Kategori"
+                    ? "white"
+                    : "text.primary",
+                "&:hover": {
+                  backgroundColor:
+                    selectedCategory === "Semua Kategori"
+                      ? "primary.dark"
+                      : "grey.300",
+                },
+              }}
+            />
+            {categories.map((category, index) => (
+              <Chip
+                key={index}
+                onClick={() => handleClick(category)}
+                size="medium"
+                label={category}
+                color={selectedCategory === category ? "primary" : "default"}
+                variant={
+                  selectedCategory === category ? "contained" : "outlined"
+                }
+                sx={{
+                  backgroundColor:
+                    selectedCategory === category
+                      ? "primary.main"
+                      : "transparent",
+                  color:
+                    selectedCategory === category ? "white" : "text.primary",
+                  "&:hover": {
+                    backgroundColor:
+                      selectedCategory === category
+                        ? "primary.dark"
+                        : "grey.300",
+                  },
+                }}
+              />
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              flexDirection: "row",
+              gap: 1,
+              width: { xs: "100%", md: "fit-content" },
+              overflow: "auto",
+            }}>
+            <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          </Box>
+        </Box>
+        {loading ? (
+          <div className="flex justify-center items-center h-full my-72">
+            <Loaders size={70} />
+          </div>
+        ) : (
+          <Grid container spacing={2} columns={12}>
+            {currentArticles.map((articles, index) => (
+              <Grid key={index} size={{ xs: 12, sm: 6, md: 4 }}>
+                <SyledCard tabIndex={0}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={`${import.meta.env.VITE_BASE_URL}/public/images/${
+                      articles.image
+                    }`}
+                    alt="image cover"
+                  />
+                  <SyledCardContent>
+                    <Typography gutterBottom variant="caption" component="div">
+                      {articles.category}
+                    </Typography>
 
-                  <StyledTypography
-                    variant="h5"
-                    component="a"
-                    href={`/articles/${articles.id}`}
-                    style={{ textDecoration: "none" }}>
-                    {articles.title}
-                  </StyledTypography>
-                  <Typography variant="body2" sx={{ pt: 2 }}>
-                    {getExcerpt(articles.content, 100)}
-                  </Typography>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: 2,
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                      pt: "16px",
-                    }}>
+                    <StyledTypography
+                      variant="h5"
+                      component="a"
+                      href={`/articles/${articles.id}`}
+                      style={{ textDecoration: "none" }}>
+                      {articles.title}
+                    </StyledTypography>
+                    <Typography variant="body2" sx={{ pt: 2 }}>
+                      {getExcerpt(articles.content, 100)}
+                    </Typography>
                     <Box
                       sx={{
                         display: "flex",
                         flexDirection: "row",
-                        gap: 1,
+                        gap: 2,
                         alignItems: "center",
+                        justifyContent: "space-between",
+                        pt: "16px",
                       }}>
-                      <Avatar
-                        key={index}
-                        src={`${import.meta.env.VITE_BASE_URL}/public/images/${
-                          users.image
-                        }`}
-                        sx={{ width: 24, height: 24 }}
-                      />
-                      <Typography variant="caption">{articles.name}</Typography>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          gap: 1,
+                          alignItems: "center",
+                        }}>
+                        <Avatar
+                          key={index}
+                          src={`${
+                            import.meta.env.VITE_BASE_URL
+                          }/public/images/${users.image}`}
+                          sx={{ width: 24, height: 24 }}
+                        />
+                        <Typography variant="caption">
+                          {articles.name}
+                        </Typography>
+                      </Box>
+                      <Typography variant="caption">
+                        {getTimeAgo(articles.updatedAt)}
+                      </Typography>
                     </Box>
-                    <Typography variant="caption">
-                      {getTimeAgo(articles.updatedAt)}
-                    </Typography>
-                  </Box>
-                </SyledCardContent>
-              </SyledCard>
-            </Grid>
-          ))}
-        </Grid>
-      )}
-      <Snackbar
-        open={alertOpen}
-        autoHideDuration={5000}
-        onClose={handleCloseAlert}>
-        <Alert
-          onClose={handleCloseAlert}
-          severity={alertSeverity}
-          sx={{ width: "100%" }}>
-          {alertMessage}
-        </Alert>
-      </Snackbar>
-    </Box>
+                  </SyledCardContent>
+                </SyledCard>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+
+        <ThemeProvider theme={defaultTheme}>
+          <Snackbar
+            open={alertOpen}
+            autoHideDuration={5000}
+            onClose={handleCloseAlert}>
+            <Alert
+              onClose={handleCloseAlert}
+              severity={alertSeverity}
+              sx={{ width: "100%" }}>
+              {alertMessage}
+            </Alert>
+          </Snackbar>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}>
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={handlePageChange}
+              color="primary"
+            />
+          </Box>
+        </ThemeProvider>
+      </Box>
+    </ThemeProvider>
   );
 }
