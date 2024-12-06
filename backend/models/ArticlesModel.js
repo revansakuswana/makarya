23b25adbe73fe8aef38ffcbff564b7905303abb9
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2024-12-07 05:32:00 +0700

    fix: update ArticlesModel to include author association and apply underscored naming convention

--- backend/models/ArticlesModel.js
+++ backend/models/ArticlesModel.js
@@ -29,10 +29,24 @@ const Articles = db.define(
       type: DataTypes.STRING(255),
       allowNull: true,
     },
+    author_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
   },
   {
+    freezeTableName: true,
     timestamps: true,
+    underscored: true,
   }
 );
+
+Articles.associate = (models) => {
+  Articles.belongsTo(models.Users, { foreignKey: "author_id", as: "author" });
+};
 
 export default Articles;
