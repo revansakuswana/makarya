eee3c45e6521e36a24e110af9f90eb2a891ee370
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2024-12-11 02:44:09 +0700

    fix: remove commented-out code and clean up backend index file

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,14 @@
 import express from "express";
-// import db from "../backend/config/database.js";
 import router from "../backend/routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-// import articleRoutes from "./routes/index.js";
 import path from "path";
 import { fileURLToPath } from "url";
-// import { scheduleJobs } from "../backend/cron/jobs.js";
-// import Users from "../backend/models/UserModel.js";
-// import Jobs from "../backend/models/JobsModel.js";
-// import UserSavedJobs from "../backend/models/UserSavedJobs.js";
 
 dotenv.config();
 
 const app = express();
-
-// try {
-//   await db.authenticate();
-//   console.log("Database Connected...");
-
-//   Users.belongsToMany(Jobs, { through: UserSavedJobs });
-//   Jobs.belongsToMany(Users, { through: UserSavedJobs });
-
-//   await db.sync();
-//   console.log("Database & tables created!");
-// } catch (error) {
-//   console.error(error);
-// }
 
 app.use(
   cors({
@@ -39,9 +20,8 @@ app.use(
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// app.use('/uploads', express.static('uploads'));
+
 app.use('/public/images', express.static(path.join(__dirname, 'public/images')));
-// app.use("/articles", articleRoutes);
 app.use(cookieParser());
 app.use(express.json());
 app.use(
